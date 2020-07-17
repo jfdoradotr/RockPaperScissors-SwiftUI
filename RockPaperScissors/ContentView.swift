4ef3dcb709e2010c7a3c3f1ab8b13c5329ae8201
Juan Dorado <fdorado985@gmail.com>
2020-07-17 11:27:47 -0500

    Basic UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,14 +7,60 @@
 
 import SwiftUI
 
+enum GameOptions: String, CaseIterable {
+  case rock = "👊"
+  case paper = "✋"
+  case scissor = "✌️"
+}
+
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!").padding()
+    ZStack {
+      Color(.systemBackground)
+        .edgesIgnoringSafeArea(.all)
+
+      VStack {
+        Text("Rock, Paper and Scissors")
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+          .padding()
+          .font(.largeTitle)
+
+        Text("Choose your option")
+          .font(.title2)
+          .fontWeight(.regular)
+          .multilineTextAlignment(.center)
+
+        Spacer()
+
+        HStack {
+          ForEach(0..<3, content: { index in
+            Button(action: {}, label: {
+              Text(GameOptions.allCases[index].rawValue)
+                .font(.system(size: 80, weight: .regular))
+                .padding()
+            })
+          })
+        }
+
+        HStack {
+          Text("Wins: 0")
+
+          Spacer()
+
+          Text("Lose: 0")
+        }
+        .padding()
+      }
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    Group {
+      ContentView()
+      ContentView().environment(\.colorScheme, .dark)
+    }
   }
 }
